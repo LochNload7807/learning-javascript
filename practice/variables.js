582c4f5dfbe99d4444573cda064b817f3871b66f
Lochen Foltz <foltzlw@s.Dcsdk12.org>
2022-11-01 16:22:28 -0600

    More Variable Practice

--- practice/variables.js
+++ practice/variables.js
@@ -7,3 +7,17 @@ let y = 276;
 let sum = x + y;
 
 let pi = 3.14;
+
+console.log("x is ", x);
+
+let name = "Lochen Foltz";
+
+let one = "1";
+
+let whatWillhappen = name + x;
+
+console.log (whatWillhappen);
+
+let isGameover = false;
+
+console.log(isGameover, typeof isGameover);
