d62e2594388e61e4888f5b42cebe08ff1d37f45d
Lochen Foltz <foltzlw@s.Dcsdk12.org>
2022-11-10 16:42:47 -0700

    strobe blocks

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,25 +1,37 @@
 
 let canvas = document.getElementById("canvas-1");
+canvas.width = 100;
+canvas.height = 100;
 /** @type {CanvasRenderingContext2D} */
 let context = canvas.getContext("2d");
 
 let squares = [];
+let gridSize = 4;
+let size = canvas.width / gridSize;
+let colors = {"red", "orange", "yellow", "green", "blue", "indigo", "violet"}
+for(let row = 1; row <= gridSize; row++ ) {
+	for(let row = 1; row <= gridSize; col++ ) {
+		let colorIndex = Math.floor( Math.random() * color.length;)
+		let colors (colorIndex);
+		drawSquare (col * size, row * size, "red", );
+	}
+}
 
-drawSquare(0, 0, "red");
-//context.fillStyle = "red";
-//context.fillRect(0, 0, 25, 25);
+// drawSquare(0, 0, "red");
+// //context.fillStyle = "red";
+// //context.fillRect(0, 0, 25, 25);
 
-drawSquare(25, 0, "blue");
-//context.fillStyle = "blue";
-//context.fillRect(25, 0, 25, 25);
+// drawSquare(25, 0, "blue");
+// //context.fillStyle = "blue";
+// //context.fillRect(25, 0, 25, 25);
 
-drawSquare(50, 0, "green");
-//context.fillStyle = "green";
-//context.fillRect(50, 0, 25, 25);
+// drawSquare(50, 0, "green");
+// //context.fillStyle = "green";
+// //context.fillRect(50, 0, 25, 25);
 
-drawSquare(75, 0, "purple");
-//context.fillStyle = "purple";
-//context.fillRect(75, 0, 25, 25);
+// drawSquare(75, 0, "purple");
+// //context.fillStyle = "purple";
+// //context.fillRect(75, 0, 25, 25);
 
 function drawSquare(x, y, color, size = 25){
 	let square = new Path2D()
