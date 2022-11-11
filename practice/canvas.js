caebedce0cfa39eb4123a49cdf09fca8945bd293
Lochen Foltz <foltzlw@s.Dcsdk12.org>
2022-11-10 18:15:41 -0700

    Crazy block edits

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,4 +1,3 @@
-
 let canvas = document.getElementById("canvas-1");
 canvas.width = 100;
 canvas.height = 100;
@@ -6,41 +5,60 @@ canvas.height = 100;
 let context = canvas.getContext("2d");
 
 let squares = [];
+
 let gridSize = 4;
+
 let size = canvas.width / gridSize;
-let colors = {"red", "orange", "yellow", "green", "blue", "indigo", "violet"}
-for(let row = 1; row <= gridSize; row++ ) {
-	for(let row = 1; row <= gridSize; col++ ) {
-		let colorIndex = Math.floor( Math.random() * color.length;)
-		let colors (colorIndex);
-		drawSquare (col * size, row * size, "red", );
-	}
-}
 
-// drawSquare(0, 0, "red");
-// //context.fillStyle = "red";
-// //context.fillRect(0, 0, 25, 25);
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+// for(let row = 1; row <= gridSize; row++ ) {
+// 	for(let col = 1; col <= gridSize; col++ ) {
+// 		let colorIndex = Math.floor( Math.random() * colors.length);
+// 		let color = colors[colorIndex];
+// 		drawSquare (col * size, row * size, color);
+// 	}
+// }
 
-// drawSquare(25, 0, "blue");
-// //context.fillStyle = "blue";
-// //context.fillRect(25, 0, 25, 25);
+function drawSquare(x, y, color, size = 25) {
+	// let square = new Path2D();
+	// square.rect(x, y, size, size);
 
-// drawSquare(50, 0, "green");
-// //context.fillStyle = "green";
-// //context.fillRect(50, 0, 25, 25);
-
-// drawSquare(75, 0, "purple");
-// //context.fillStyle = "purple";
-// //context.fillRect(75, 0, 25, 25);
-
-function drawSquare(x, y, color, size = 25){
-	let square = new Path2D()
-	square.rectrect(x, y, size, size)
-    
-	squares.push(square)
-	
+	// squares.push(square);
+	// console.log(x, y, color, size);
 	context.fillStyle = color;
 	context.fillRect(x, y, size, size);
 }
 
 console.log(squares);
+
+let currentTime = 0;
+const refreshRate = 500;
+let lastRefresh = 0;
+
+function drawLoop(timestamp) {
+	let elaspedTime = timestamp - currentTime;
+	currentTime = timestamp;
+
+	lastRefresh = lastRefresh + elaspedTime;
+
+	// console.log(lastRefresh, timestamp, elaspedTime);
+
+	if (lastRefresh >= refreshRate) {
+
+		
+		lastRefresh = 0;
+		for (let row = 0; row < gridSize; row++) {
+			for (let col = 0; col < gridSize; col++) {
+
+				let colorIndex = Math.floor(Math.random() * colors.length);
+				let color = colors[colorIndex];
+
+				drawSquare(col * size, row * size, color, size);
+			}
+		}
+		
+	}
+	requestAnimationFrame(drawLoop);
+}
+
+requestAnimationFrame(drawLoop);
