165beae0424a361e6ca5269a8a34c845e6a80685
Lochen Foltz <foltzlw@s.Dcsdk12.org>
2022-11-15 16:20:03 -0700

    11/15

--- practice/canvas.js
+++ practice/canvas.js
@@ -33,68 +33,56 @@ class Clickbox {
 
         if(this.lastRefresh < this.refreshRate) return;
 
-		if(this.lastRefresh >= this.refreshRate){
-			this.setColor
-		}
+
+
+		this.lastRefresh = 0;
+
+		this.setColor();
+		
 	}
 
-	draw() {}
+	draw() {
+		context.fillStyle = this.color;
+	context.fillRect(this.x, this.y, this.size, this.size);
+	}
 }
 
 let squares = [];
 
 let gridSize = 4;
-
 let size = canvas.width / gridSize;
-
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-// for(let row = 1; row <= gridSize; row++ ) {
-// 	for(let col = 1; col <= gridSize; col++ ) {
-// 		let colorIndex = Math.floor( Math.random() * colors.length);
-// 		let color = colors[colorIndex];
-// 		drawSquare (col * size, row * size, color);
-// 	}
-// }
 
-function drawSquare(x, y, color, size = 25) {
-	// let square = new Path2D();
-	// square.rect(x, y, size, size);
-
-	// squares.push(square);
-	// console.log(x, y, color, size);
-	context.fillStyle = color;
-	context.fillRect(x, y, size, size);
+for(let row = 1; row <= gridSize; row++ ) {
+	for(let col = 1; col <= gridSize; col++ ) {
+		//let colorIndex = Math.floor( Math.random() * colors.length);
+		//let color = colors[colorIndex];
+		//drawSquare (col * size, row * size, color, size);
+ 
+		let x = col * size;
+		let y = row * size
+		let box = new Clickbox(x, y, size, colors);
+		squares.push(box);
+	}
 }
 
-console.log(squares);
+
 
 let currentTime = 0;
-const refreshRate = 500;
-let lastRefresh = 0;
+
 
 function drawLoop(timestamp) {
 	let elaspedTime = timestamp - currentTime;
 	currentTime = timestamp;
-
-	lastRefresh = lastRefresh + elaspedTime;
-
-	// console.log(lastRefresh, timestamp, elaspedTime);
-
-	if (lastRefresh >= refreshRate) {
-
-		
-		lastRefresh = 0;
-		for (let row = 0; row < gridSize; row++) {
-			for (let col = 0; col < gridSize; col++) {
-
-				let colorIndex = Math.floor(Math.random() * colors.length);
-				let color = colors[colorIndex];
-
-				drawSquare(col * size, row * size, color, size);
-			}
-		}
-		
-	}
+	
+    squares.forEach((b) => {
+        b.update(elaspedTime);
+		b.draw();
+	}); 
+	
+			
+	
+	
 	requestAnimationFrame(drawLoop);
 }
 
